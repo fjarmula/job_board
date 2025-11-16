ba77c87f9a182feb7b06351d695c7290bb3dfc0b
fjarmula <jarmulafilip@gmail.com>
2025-11-16 21:38:09 +0100

    Add email validation and reset session before new session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end

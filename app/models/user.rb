b7df9441990df778ff94dad19e0a5786769ad026
fjarmula <jarmulafilip@gmail.com>
2025-11-16 21:50:18 +0100

    Add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
-  validates :email,
+  validates :email_address,
             presence: true,
             uniqueness: true,
             format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+  validates :password,
+            presence: true,
+            length: { minimum: 6 }
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end

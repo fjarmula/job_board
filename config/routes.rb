18a72bb0f993597b89f1b9d8b5424e57d6a451cd
fjarmula <jarmulafilip@gmail.com>
2025-11-17 11:43:34 +0100

    Add health check route for Kamal

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,8 @@ Rails.application.routes.draw do
 
   # Root
   root "home#index"
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
 end

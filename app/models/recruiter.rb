79929e507e5130f6e2a17ce3e3354202b51229c9
fjarmula <jarmulafilip@gmail.com>
2025-11-25 21:06:53 +0100

    Recruiter can sign up

---
+++ app/models/recruiter.rb
@@ -0,0 +1,11 @@
+class Recruiter < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Validations
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :company_name, presence: true
+end

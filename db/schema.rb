7d718534faa201ac7b0585a519b64b52cf2a76ef
fjarmula <jarmulafilip@gmail.com>
2025-11-27 18:27:38 +0100

    Add views for company and recruiter

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_27_103115) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_27_161929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "companies", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.text "description"
+    t.integer "founded_in"
+    t.string "headquarters"
+    t.string "industry"
     t.string "name", null: false
+    t.string "size"
     t.datetime "updated_at", null: false
+    t.string "website"
     t.index ["name"], name: "index_companies_on_name", unique: true
   end
 

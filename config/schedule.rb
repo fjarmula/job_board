92dfa0eb0ca6038a90cfc50b81a3e135dcf4c3ed
fjarmula <jarmulafilip@gmail.com>
2025-12-22 19:26:53 +0100

    Remove not needed lines

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,10 +19,6 @@
 
 # Learn more: http://github.com/javan/whenever
 
-set :output, "log/cron.log"
-set :environment, "production"
-set :job_template, "/bin/bash -l -c ':job'"
-
 every 1.day, at: '12:00 am' do
   runner "BulldogCrawler.new(pages: 3).call"
 end
